ffa951d13803cb3f44d11937934ca8a78eeb0a70
Aldirhezaldi <aldirhezaldi339@gmail.com>
2021-02-26 11:31:15 +0700

    push pertemuan 3

---
+++ Pertemuan 3/Pertemuan3(1)/Assets/Script/Player.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class Player : MonoBehaviour
+{
+    public Text starText;
+    private bool carryingStar = false; void Start()
+    {
+        UpdateStarText();
+    }
+    void OnTriggerEnter2D(Collider2D hit)
+    {
+        if (hit.CompareTag("Ruby"))
+        {
+            carryingStar = true; UpdateStarText(); Destroy(hit.gameObject);
+        }
+    }
+    private void UpdateStarText()
+    {
+        string starMessage = "Tidak  Bawa  Ruby  :-(";
+        if (carryingStar) starMessage = "Bawa  Ruby  :-)"; starText.text = starMessage;
+    }
+}
---
+++ Pertemuan 3/Pertemuan3(1)/Assets/Script/PlayerInventoryDisplay.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerInventoryDisplay : MonoBehaviour
+{
+    public Text starText;
+    public void OnChangeCarryingStar(bool carryingStar)
+    {
+        string starMessage = "Tidak  Bawa  Ruby  :-(";
+        if (carryingStar) starMessage = "Bawa  Ruby  :-)"; starText.text = starMessage;
+    }
+}
