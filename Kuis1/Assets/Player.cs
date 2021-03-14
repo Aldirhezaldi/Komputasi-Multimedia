145eba6609667004511a0ea1d411d6a87dd41380
Aldirhezaldi <aldirhezaldi339@gmail.com>
2021-03-14 17:46:24 +0700

    push kuis

---
+++ Kuis1/Assets/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    private PlayerInventoryDisplay playerInventoryDisplay;
+
+    public Text starText;
+
+    private int totalStars = 0;
+
+    void Start()
+    {
+        playerInventoryDisplay = GetComponent<PlayerInventoryDisplay>();
+        UpdateStarText();
+    }
+
+    void OnTriggerEnter2D(Collider2D hit)
+    {
+        if (hit.CompareTag("Ruby"))
+        {
+            totalStars++;
+            UpdateStarText();
+            playerInventoryDisplay.OnChangeStarTotal (totalStars);
+            Destroy(hit.gameObject);
+        }
+    }
+
+    private void UpdateStarText()
+    {
+        string starMessage = "Ruby = " + totalStars;
+        starText.text = starMessage;
+    }
+}
---
+++ Kuis1/Assets/PlayerTutor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerTutor : MonoBehaviour
+{
+    public Text starText;
+
+    private int totalStars = 0;
+
+    void Start()
+    {
+        UpdateStarText();
+    }
+
+    void OnTriggerEnter2D(Collider2D hit)
+    {
+        if (hit.CompareTag("Ruby"))
+        {
+            totalStars++;
+            UpdateStarText();
+            Destroy(hit.gameObject);
+        }
+    }
+
+    private void UpdateStarText()
+    {
+        string starMessage = "Ruby = " + totalStars;
+        starText.text = starMessage;
+    }
+}
