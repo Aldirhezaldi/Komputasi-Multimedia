59d17a459db37a1fa9ebcd139bc488f142893de8
Aldirhezaldi <aldirhezaldi339@gmail.com>
2021-03-26 17:58:33 +0700

    push week 7

---
+++ Pertemuan 7/Pertemuan 7.2/Assets/CamFollows.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFollows : MonoBehaviour
+{
+    public Transform ikut;
+
+    void Update(){
+        transform.LookAt(ikut);
+    }
+}
